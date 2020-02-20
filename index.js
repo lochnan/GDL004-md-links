6cfb8a7d444ff3eebbd0822c7c386a50c328062f
Nancy <lochnan@99.200-57-1.bestelclientes.com.mx>
2020-02-20 09:18:25 -0600

    Obteniento tokens

---
+++ index.js
@@ -0,0 +1,65 @@
+// 1. Oteniendo el contenido del archivo con el módulo fs
+
+// Lamando el módulo de fs (File System)
+const fs = require('fs')
+const path = require('path')
+// Llamadno el módulo de MarkdownIt
+const md = require('markdown-it')({
+  html: true,
+  linkify: true,
+  typographer: true
+});
+
+// Cuando el usuario coloca el archivo que se ejecuta, obteniendo el path
+// let pathToFile =  process.argv[1];
+// Obteniendo el nombre del archivo que el usuario quiere revisar
+
+let searchExtension = process.argv[2];
+//console.log (searchExtension)
+
+// Si el archivo no es de extensión .md no lo abras, manda un error
+if (path.extname(searchExtension) === '.md') {
+
+  // Obteniendo el contenido del documento. Se coloca la codificación
+  fs.readFile(searchExtension, 'utf8', (err, data) => {
+    if (err) {
+      return console.log(err + 'Tu archivo no es ".md"!')
+    }
+    // Si no hay un error entonces obtén el contenido
+    let content = data; // Es un string
+    // console.log(typeof content)
+    // console.log(content)
+
+    /* Aplicando el método de render de la library markdown-It y obtengo
+    el un string con HTML */
+    let result = md.parse(content, {});
+    console.log(result)
+
+   // Si obtengo tokens, ¿tengo que iterar en ellos para obtener los links?
+   // Guardalos en un objeto
+
+  })
+}
+else {
+  console.log('Tu archivo' + searchExtension + 'no es un .md ')
+}
+
+//console.log(contentPath)
+
+2.//  En el callback 
+/*
+Los links se identifican en un archivo md como:
+[Texto] ()
+<a href> </>
+https://
+
+//3.  Función que exportará la validación y los stats
+Los módulos se exportan con module.exports.hello = hello;
+function mdLinks (path, opts) {
+   Aquí la función que importa el módulo que obtiene las opts (verify and stats)
+    Para verify se implementaría: ¿un if que valorara si los links en el array son válidos?
+    && opts.validate para mostrar los errores (¿Es npm class validator? Revisar  https://www.npmjs.com/package/class-validator)
+    Para stats ¿se implementaría un contador de los links funcionales y no funcionales después de la validación
+}
+return
+*/
---
+++ prueba.md
@@ -0,0 +1,8 @@
+
+## Links
+
+[link text](http://dev.nodeca.com)
+
+[link with title](http://nodeca.github.io/pica/demo/ "title text!")
+
+Autoconverted link https://github.com/nodeca/pica (enable linkify to see)
